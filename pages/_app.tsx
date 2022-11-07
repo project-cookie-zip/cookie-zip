10ca035debe8a7faf2aa5e327aab7c5cb473c3d7
LEE-YO-HAN <yhl0078@gmail.com>
2022-11-07 14:29:57 +0900

    set theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { useState,useEffect } from "react";
+import { darkTheme, lightTheme } from "../src/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isDark, setIsDark] = useState(false);
+
+
+  
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import {DefaultTheme} from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+
+};
+
+
+export const lightTheme: DefaultTheme = {
+};
+
+
+
