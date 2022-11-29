fca739326f474b2c8fecdb3bb39499b99a917401
LEE-YO-HAN <yhl0078@gmail.com>
2022-11-29 13:32:23 +0900

    add loading spinner , loading status(_app.tsx)  /  style fix videos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,17 +6,21 @@ import { useState, useEffect } from "react";
 import { darkTheme, lightTheme } from "../src/theme";
 import { Layout } from "../src/components/layout/Layout";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { useLoading } from "src/hooks/useLoading";
+import { LoadingSpinner } from "src/components/videos/video/LoadingSpinner";
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState<boolean>(false);
+  const isLoading: boolean = useLoading();
 
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
           <Layout>
-            <Component {...pageProps} />
+            {isLoading ? <LoadingSpinner /> : <Component {...pageProps} />}
+            {/* <Component {...pageProps} /> */}
           </Layout>
         </ThemeProvider>
       </RecoilRoot>
