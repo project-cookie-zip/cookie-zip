98ba3f829c1cfd9489c98e9c82b9b3ec59c281d7
jeongwon <rladnjs15@naver.com>
2022-12-05 23:13:51 +0900

    Feat: mypage 이미지 업로드

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import { baseImageData } from "@utils/client/baseImage";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function MyPage() {
   const getMyInfo = async () => {
@@ -21,6 +22,51 @@ export default function MyPage() {
 
   const baseImage = baseImageData(data?.id);
 
+  //userImage 업로드
+
+  const [testSend, setTestSend] = useState("");
+
+  const uploadImage = (file: any) => {
+    setTestSend(file);
+  };
+
+  const addImage = async () => {
+    let formData = new FormData();
+    formData.append("file", testSend);
+
+    const {
+      data: { id, uploadURL },
+    } = await axios.get("/api/image");
+    // console.log(data);
+    // console.log(uploadURL, id);
+    const data1 = await axios
+      .post(uploadURL, formData)
+      .then(res => console.log(res));
+    console.log("data1", data1);
+
+    const data = await (
+      await fetch(
+        // `https://api.cloudflare.com/client/v4/accounts/e564ea5cae1cb0fb8004a589abe35f63/stream/${id}`,
+        // `https://api.cloudflare.com/cdn-cgi/imagedelivery/e564ea5cae1cb0fb8004a589abe35f63/${id}/public`,
+        `https://api.cloudflare.com/client/v4/accounts/e564ea5cae1cb0fb8004a589abe35f63/images/v2/${id}`,
+        {
+          // method: "GET",
+          headers: {
+            Authorization: `Bearer 2PptoKx0NQLjSOoPxLgePH2I6GztAIx1p-hKv6Z0`,
+            "Content-Type": "application/json",
+          },
+        },
+      )
+    ).json();
+    console.log(data);
+    // await axios.post("/api/videos", {
+    //   title,
+    //   description: content,
+    //   videoUrl: preview,
+    //   thumbnailUrl: thumbnail,
+    // });
+  };
+
   return (
     <>
       <STTopBar>
@@ -29,13 +75,32 @@ export default function MyPage() {
         <STMenu>채널정보</STMenu>
       </STTopBar>
       <STProfile>
-        <Image
-          src={data?.avatar ? data?.avatar : baseImage}
-          alt="프로필사진"
-          width={60}
-          height={60}
-          unoptimized={true}
-        />
+        <STAvatarBox>
+          <Image
+            src={data?.avatar ? data?.avatar : baseImage}
+            alt="프로필사진"
+            width={60}
+            height={60}
+            unoptimized={true}
+          />
+          <Image
+            className="edit"
+            src={require("../../src/images/cookieEdit.png")}
+            alt="https://icons8.com/icon/WLubF9R6RUtV/upload-to-cloud Upload to Cloud icon by https://icons8.com Icons8"
+            width={25}
+            height={25}
+          />
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e: any) => {
+              uploadImage(e.target.files[0]);
+            }}
+          />
+          <button onClick={addImage} type="button">
+            사진 업로드
+          </button>
+        </STAvatarBox>
         <span className="nickname">{data?.name}</span>
         <div>
           <span>구독자 10명 ∙ 동영상 10개</span>
@@ -70,6 +135,16 @@ const STProfile = styled.div`
   }
   .nickname {
     font-size: 28px;
+  }
+`;
+
+const STAvatarBox = styled.span`
+  display: flex;
+  position: relative;
+  .edit {
+    position: absolute;
+    bottom: 0;
+    right: -28px;
   }
 `;
 
