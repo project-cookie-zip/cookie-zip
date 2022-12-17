8dcfe332f1a673257c7c4cdbe145f87876d6d42b
LEE-YO-HAN <yhl0078@gmail.com>
2022-12-17 19:27:31 +0900

    CannotDesktop.tsx style fix

--- src/hooks/useMideaQuery.tsx
+++ src/hooks/useMideaQuery.tsx
@@ -22,7 +22,7 @@ export const Mobile = () => {
 
   useEffect(() => {
     checkResize();
-    window.addEventListener(`resize`, checkResize);
+    // window.addEventListener(`resize`, checkResize);
   }, [isMobile]);
 
   return mobile;
