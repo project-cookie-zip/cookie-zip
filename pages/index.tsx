f2eef95c38468f392f2520399ee281f67a15e925
LEE-YO-HAN <yhl0078@gmail.com>
2022-12-26 23:35:30 +0900

    Update: add meta:og tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,12 @@ export default function Home() {
           name="hello cookieZIP"
           content="upload your videos and watching!"
         />
+        <meta property="og:title" content="Cookie-zip" />
+        <meta property="og:description" content="쿠키영상 심플하게 즐기자!" />
+        <meta
+          property="og:image"
+          content="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FxuqSn%2FbtrUDiTCMEz%2FuK5c7jywvxJnKeyu6VRaK1%2Fimg.png"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <List />
